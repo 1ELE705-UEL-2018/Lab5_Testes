c08c5d1fee9d461eea098427c341d86f520b559b
Décio Luiz Gazzoni Filho <decio@decpp.net>
2018-10-10 14:48:41 -0300

    Correção de erros nos testes do exercício 6.

--- testes_ll_inserir_elemento_apos.cpp
+++ testes_ll_inserir_elemento_apos.cpp
@@ -28,25 +28,43 @@ TEST(ll_inserir_elemento_apos, ListaVazia)
 	aux_ll_limpar(&ll_depois);
 }
 
-TEST(ll_inserir_elemento_apos, ListaDoisElementosInsereInicio)
+TEST(ll_inserir_elemento_apos, ListaUmElementoInsereInicio)
 {
 	struct lld ll, ll_depois;
 	int dados[2] = { 0, 1 }, dados_depois[2] = { 0, 1 };
+	struct lld_el* elementos[sizeof(dados) / sizeof(int) - 1];
 	struct lld_el* elementos_depois[sizeof(dados_depois)/sizeof(int)];
-	struct lld_el* a_inserir[2];
+	struct lld_el* a_inserir[1];
 
-	ll.inicio = ll.fim = NULL;
-	ll.compar = intcmp;
+	aux_ll_preencher(&ll, intcmp, dados + 1, sizeof(dados) / sizeof(int) - 1, elementos);
+	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois) / sizeof(int), elementos_depois);
 
 	a_inserir[0] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[1] = (struct lld_el*)malloc(sizeof(struct lld_el));
+	a_inserir[0]->dado = &dados[0];
+
+	ll_inserir_elemento_apos(&ll, NULL, a_inserir[0]);
+
+	ASSERT_TRUE(LinkedListsMatch(&ll, &ll_depois));
+
+	aux_ll_limpar(&ll);
+	aux_ll_limpar(&ll_depois);
+}
+
+TEST(ll_inserir_elemento_apos, ListaUmElementosInsereFim)
+{
+	struct lld ll, ll_depois;
+	int dados[2] = { 0, 1 }, dados_depois[2] = { 0, 1 };
+	struct lld_el* elementos[sizeof(dados) / sizeof(int) - 1];
+	struct lld_el* elementos_depois[sizeof(dados_depois)/sizeof(int)];
+	struct lld_el* a_inserir[1];
+
+	aux_ll_preencher(&ll, intcmp, dados, sizeof(dados) / sizeof(int) - 1, elementos);
+	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois) / sizeof(int), elementos_depois);
+
+	a_inserir[0] = (struct lld_el*)malloc(sizeof(struct lld_el));
 	a_inserir[0]->dado = &dados[1];
-	a_inserir[1]->dado = &dados[0];
 
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[0]);
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[1]);
-
-	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois)/sizeof(int), elementos_depois);
+	ll_inserir_elemento_apos(&ll, ll.inicio, a_inserir[0]);
 
 	ASSERT_TRUE(LinkedListsMatch(&ll, &ll_depois));
 
@@ -54,91 +72,7 @@ TEST(ll_inserir_elemento_apos, ListaDoisElementosInsereInicio)
 	aux_ll_limpar(&ll_depois);
 }
 
-TEST(ll_inserir_elemento_apos, ListaDoisElementosInsereFim)
-{
-	struct lld ll, ll_depois;
-	int dados[2] = { 0, 1 }, dados_depois[2] = { 0, 1 };
-	struct lld_el* elementos_depois[sizeof(dados_depois)/sizeof(int)];
-	struct lld_el* a_inserir[2];
-
-	ll.inicio = ll.fim = NULL;
-	ll.compar = intcmp;
-
-	a_inserir[0] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[1] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[0]->dado = &dados[0];
-	a_inserir[1]->dado = &dados[1];
-
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[0]);
-	ll_inserir_elemento_apos(&ll, ll.inicio, a_inserir[1]);
-
-	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois)/sizeof(int), elementos_depois);
-
-	ASSERT_TRUE(LinkedListsMatch(&ll, &ll_depois));
-
-	aux_ll_limpar(&ll);
-	aux_ll_limpar(&ll_depois);
-}
-
-TEST(ll_inserir_elemento_apos, ListaTresElementosInsereInicioInicio)
-{
-	struct lld ll, ll_depois;
-	int dados[3] = { 0, 1, 2 }, dados_depois[3] = { 0, 1, 2 };
-	struct lld_el* elementos_depois[sizeof(dados_depois)/sizeof(int)];
-	struct lld_el* a_inserir[3];
-
-	ll.inicio = ll.fim = NULL;
-	ll.compar = intcmp;
-
-	a_inserir[0] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[1] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[2] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[0]->dado = &dados[2];
-	a_inserir[1]->dado = &dados[1];
-	a_inserir[2]->dado = &dados[0];
-
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[0]);
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[1]);
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[2]);
-
-	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois)/sizeof(int), elementos_depois);
-
-	ASSERT_TRUE(LinkedListsMatch(&ll, &ll_depois));
-
-	aux_ll_limpar(&ll);
-	aux_ll_limpar(&ll_depois);
-}
-
-TEST(ll_inserir_elemento_apos, ListaTresElementosInsereInicioMeioFim)
-{
-	struct lld ll, ll_depois;
-	int dados[3] = { 0, 1, 2 }, dados_depois[3] = { 0, 1, 2 };
-	struct lld_el* elementos_depois[sizeof(dados_depois)/sizeof(int)];
-	struct lld_el* a_inserir[3];
-
-	ll.inicio = ll.fim = NULL;
-	ll.compar = intcmp;
-
-	a_inserir[0] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[1] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[2] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[0]->dado = &dados[0];
-	a_inserir[1]->dado = &dados[2];
-	a_inserir[2]->dado = &dados[1];
-
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[0]);
-	ll_inserir_elemento_apos(&ll, ll.fim, a_inserir[1]);
-	ll_inserir_elemento_apos(&ll, ll.inicio, a_inserir[2]);
-
-	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois)/sizeof(int), elementos_depois);
-
-	ASSERT_TRUE(LinkedListsMatch(&ll, &ll_depois));
-
-	aux_ll_limpar(&ll);
-	aux_ll_limpar(&ll_depois);
-}
-
-TEST(ll_inserir_elemento_apos, ListaTresElementosInsereMeio)
+TEST(ll_inserir_elemento_apos, ListaDoisElementosInsereMeio)
 {
 	struct lld ll, ll_depois;
 	int dados[3] = { 0, 2, 1 }, dados_depois[3] = { 0, 1, 2 };
@@ -146,47 +80,13 @@ TEST(ll_inserir_elemento_apos, ListaTresElementosInsereMeio)
 	struct lld_el* elementos_depois[sizeof(dados_depois)/sizeof(int)];
 	struct lld_el* a_inserir[1];
 
-	aux_ll_preencher(&ll, intcmp, dados, sizeof(dados)/sizeof(int), elementos);
+	aux_ll_preencher(&ll, intcmp, dados, sizeof(dados)/sizeof(int) - 1, elementos);
+	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois) / sizeof(int), elementos_depois);
 
 	a_inserir[0] = (struct lld_el*)malloc(sizeof(struct lld_el));
 	a_inserir[0]->dado = &dados[2];
 
-	aux_ll_preencher(&ll, intcmp, dados, 2, elementos);
 	ll_inserir_elemento_apos(&ll, ll.inicio, a_inserir[0]);
-
-	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois)/sizeof(int), elementos_depois);
-
-	ASSERT_TRUE(LinkedListsMatch(&ll, &ll_depois));
-
-	aux_ll_limpar(&ll);
-	aux_ll_limpar(&ll_depois);
-}
-
-TEST(ll_inserir_elemento_apos, ListaQuatroElementosInserePrimeiroSegundoTerceiroQuarto)
-{
-	struct lld ll, ll_depois;
-	int dados[4] = { 0, 1, 2, 3 }, dados_depois[4] = { 0, 1, 2, 3 };
-	struct lld_el* elementos_depois[sizeof(dados_depois)/sizeof(int)];
-	struct lld_el* a_inserir[4];
-
-	ll.inicio = ll.fim = NULL;
-	ll.compar = intcmp;
-
-	a_inserir[0] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[1] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[2] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[3] = (struct lld_el*)malloc(sizeof(struct lld_el));
-	a_inserir[0]->dado = &dados[0];
-	a_inserir[1]->dado = &dados[1];
-	a_inserir[2]->dado = &dados[2];
-	a_inserir[3]->dado = &dados[3];
-
-	ll_inserir_elemento_apos(&ll, NULL, a_inserir[0]);
-	ll_inserir_elemento_apos(&ll, ll.inicio, a_inserir[1]);
-	ll_inserir_elemento_apos(&ll, ll.inicio->prox, a_inserir[2]);
-	ll_inserir_elemento_apos(&ll, ll.fim, a_inserir[3]);
-
-	aux_ll_preencher(&ll_depois, intcmp, dados_depois, sizeof(dados_depois)/sizeof(int), elementos_depois);
 
 	ASSERT_TRUE(LinkedListsMatch(&ll, &ll_depois));
 
